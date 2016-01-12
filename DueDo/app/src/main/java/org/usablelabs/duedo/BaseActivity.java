d7491cd066746eeed68b99511f3d97844dabcf55
Thawatchai Piyawat <thawatchai@usablelabs.org>
2016-01-12 20:53:26 +0700

    add blank activities for CRUD

---
+++ DueDo/app/src/main/java/org/usablelabs/duedo/BaseActivity.java
@@ -0,0 +1,12 @@
+package org.usablelabs.duedo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ DueDo/app/src/main/java/org/usablelabs/duedo/FormActivity.java
@@ -0,0 +1,12 @@
+package org.usablelabs.duedo;
+
+import android.os.Bundle;
+
+public class FormActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+    }
+}
