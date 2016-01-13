aa6ed0cf39ce22d44b29d270db82ed34bf59a9a2
Thawatchai Piyawat <thawatchai@usablelabs.org>
2016-01-13 20:32:01 +0700

    add main toolbar; newFab button in ListActivity

--- DueDo/app/src/main/java/org/usablelabs/duedo/BaseActivity.java
+++ DueDo/app/src/main/java/org/usablelabs/duedo/BaseActivity.java
@@ -1,12 +1,27 @@
 package org.usablelabs.duedo;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.mikepenz.iconics.IconicsDrawable;
+import com.mikepenz.iconics.typeface.IIcon;
 
 public abstract class BaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    protected void setDrawer(Boolean upEnabled) {
+        Toolbar toolbar_main = (Toolbar) findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbar_main);
+    }
+
+    protected Drawable buildDrawable(IIcon icon) {
+        return new IconicsDrawable(this).icon(icon).color(Color.WHITE).sizeDp(24).paddingDp(4);
     }
 }
--- DueDo/app/src/main/java/org/usablelabs/duedo/ListActivity.java
+++ DueDo/app/src/main/java/org/usablelabs/duedo/ListActivity.java
@@ -1,12 +1,31 @@
 package org.usablelabs.duedo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.view.View;
+
+import com.mikepenz.material_design_iconic_typeface_library.MaterialDesignIconic;
 
 public class ListActivity extends BaseActivity {
+
+    private static int NEW_TASK = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        setDrawer(false);
+        setTitle(R.string.tasks);
+
+        FloatingActionButton newFab = (FloatingActionButton) findViewById(R.id.newFab);
+        newFab.setImageDrawable(buildDrawable(MaterialDesignIconic.Icon.gmi_plus));
+        newFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ListActivity.this, FormActivity.class);
+                startActivityForResult(intent, NEW_TASK);
+            }
+        });
     }
 }
